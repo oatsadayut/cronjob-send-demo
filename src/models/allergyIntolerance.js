97c83d17b5747ac0220f5a4d6c8278cd147a674e
ice <40240716+oatsadayut@users.noreply.github.com>
2024-02-06 23:24:23 +0700

    Add models opject Allergy Intolerance

---
+++ src/models/allergyIntolerance.js
@@ -0,0 +1,124 @@
+export const AllergyIntoleranceOpject = {
+  managingOrganization: {
+    type: "Organization",
+    identifier: {
+      use: "official",
+      system: "https://bps.moph.go.th/hcode/5",
+      value: "99999",
+    },
+    display: "โรงพยาบาลทดสอบระบบ",
+  },
+  identifier: [
+    {
+      system: "http://acme.com/ids/patients/risks",
+      value: "49476535",
+    },
+  ],
+  clinicalStatus: {
+    coding: [
+      {
+        system:
+          "http://terminology.hl7.org/CodeSystem/allergyintolerance-clinical",
+        code: "active",
+        display: "Active",
+      },
+    ],
+  },
+  verificationStatus: {
+    coding: [
+      {
+        system:
+          "http://terminology.hl7.org/CodeSystem/allergyintolerance-verification",
+        code: "confirmed",
+        display: "Confirmed",
+      },
+    ],
+  },
+  type: "allergy",
+  category: [
+    // กรณีแพ้อาหาร ระบุเป็น "food"
+    "medication",
+  ],
+  criticality: "high",
+  code: {
+    coding: [
+      {
+        system: "https://phr1.moph.go.th/api/CodingSystem?System=tmt_substance",
+        code: "579224",
+        display: "PENICILLIN G SODIUM",
+      },
+    ],
+  },
+  seriousness: {
+    coding: [
+      {
+        system:
+          "https://phr1.moph.go.th/api/CodingSystem?System=allergy_seriousness",
+        code: "1",
+        display: "ไม่ร้ายแรง",
+      },
+    ],
+  },
+  allergyGroup: {
+    coding: [
+      {
+        system: "https://phr1.moph.go.th/api/CodingSystem?System=allergy_group",
+        code: "1",
+        display: "Cardiovascular diaorder",
+      },
+    ],
+  },
+  allergyInformationSource: {
+    coding: [
+      {
+        system:
+          "https://phr1.moph.go.th/api/CodingSystem?System=allergy_source",
+        code: "1",
+        display: "มาโรงพยาบาล",
+      },
+    ],
+  },
+  allergyResult: {
+    coding: [
+      {
+        system:
+          "https://phr1.moph.go.th/api/CodingSystem?System=allergy_result",
+        code: "1",
+        display: "หายเป็นปกติ",
+      },
+    ],
+  },
+  naranjoResult: {
+    coding: [
+      {
+        system:
+          "https://phr1.moph.go.th/api/CodingSystem?System=allergy_naranjo_result",
+        code: "1",
+        display: "Definite - ใช่แน่",
+      },
+    ],
+  },
+  preventable: false,
+  preventableScore: 1,
+  recordedDate: "2015-08-06T15:37:31-06:00",
+  recordOfficer: {
+    reference: "Practitioner/ว000099",
+    identifier: "ว000099",
+    display: "นพ.ทดสอบ ระบบ 2",
+  },
+  reaction: [
+    {
+      manifestation: [
+        {
+          coding: [
+            {
+              system: "http://snomed.info/sct",
+              code: "247472004",
+              display: "Hives",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
