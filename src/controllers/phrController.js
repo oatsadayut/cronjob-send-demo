238d8e9b07c02584a99297d313a41f06372c4343
ice <40240716+oatsadayut@users.noreply.github.com>
2024-02-07 23:17:19 +0700

    Updata data obj

--- src/controllers/phrController.js
+++ src/controllers/phrController.js
@@ -1,19 +1,19 @@
 import { http } from "../config/http.js";
-import { getPatient, getAllergy } from "../services/phrService.js";
+import { getPatientData } from "../services/phrService.js";
 
 const sendPatientData = async () => {
   try {
-    const patient = await getPatient();
-    const allergy = await getAllergy();
+    const patient = await getPatientData();
 
     const payload = {
+      code: "00000",
+      name: "โรงพยาบาลทดสอบ",
       patient: patient,
-      allergy: allergy,
     };
 
     const res = await http.post("{* api path *}", payload);
     const result = res.data;
-    console.log(result);
+    console.log(payload);
 
   } catch (error) {
     console.error(error.message);
--- src/services/phrService.js
+++ src/services/phrService.js
@@ -1,23 +1,27 @@
 import knex from "../config/database.js";
 
-const getPatient = async () => {
-  //Mock data
-  let mockData = {
-    cid: "0000000000000",
-    fullname: "นายทดสอบ ใจดี",
-  };
-
-  return mockData;
-};
-
-const getAllergy = async () => {
+const getPatientData = async () => {
   //Mock data
   let mockData = [
-    { name: "พารา", group: "ยาแก้ปวด", type: "drug" },
-    { name: "แอนตาซีน", group: "ยาแก้ปวด", type: "drug" },
-    { name: "หูฉลาม", group: "อาหาร", type: "food" },
+    {
+      cid: "0000000000000",
+      fullname: "นายทดสอบ ใจดี",
+      allergy: [
+        { name: "พารา", group: "ยาแก้ปวด", type: "drug" },
+        { name: "แอนตาซีน", group: "ยาแก้ปวด", type: "drug" },
+        { name: "หูฉลาม", group: "อาหาร", type: "food" },
+      ],
+    },
+    {
+      cid: "3300000000001",
+      fullname: "นายทดสอบ2 สุขสม",
+      allergy: [
+        { name: "แอนตาซีน", group: "ยาแก้ปวด", type: "drug" },
+        { name: "หูฉลาม", group: "อาหาร", type: "food" },
+      ],
+    },
   ];
   return mockData;
 };
 
-export { getAllergy, getPatient };
+export { getPatientData };
